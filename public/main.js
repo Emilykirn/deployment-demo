5e051159633ba4afaf49a3948ca3b3a4f993ba53
Emily Kirn <emilykirn@gmail.com>
2022-11-23 15:04:41 -0700

    added new url

--- public/main.js
+++ public/main.js
@@ -3,3 +3,16 @@ const btn = document.querySelector('button')
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+
+const button2 = document.querySelector('#button2')
+
+const getCat = () => {
+    axios.get('http://ec2-52-10-119-210.us-west-2.compute.amazonaws.com/api/cat')
+    .then(res => {
+        alert(res.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+button2.addEventListener('click', getCat)
